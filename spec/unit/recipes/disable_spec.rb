1671d85d034240daeb7e07384dc86878d1d1909a
Charles Johnson <charles@opscode.com>
2013-08-29 15:49:46 -0700

    Cleaned up disable_spec.rb
    In the process discovered errors in disable.rb, refactored

--- spec/unit/recipes/disable_spec.rb
+++ spec/unit/recipes/disable_spec.rb
@@ -2,25 +2,25 @@ require 'spec_helper'
 
 describe "ntp::disable" do
   let(:chef_run) do
-    runner = ChefSpec::ChefRunner.new(
-      log_level: :error,
-    )
-    Chef::Config.force_logger true
+    runner = ChefSpec::ChefRunner.new()
     runner.converge('recipe[ntp::disable]')
   end
 
-# Standard test set
   it "halts the ntp service" do
     expect(chef_run).to stop_service "ntp"
     expect(chef_run).to set_service_to_not_start_on_boot 'ntp'
   end
 
-  pending "TODO: Fix the content match test" do
-    it "Creates the /etc/default/ntpdate file" do
-      # TODO: Fix this match so it actually works
-      (chef_run).to create_file_with_content '/etc/default/ntpdate', "exit 0"
+  context "Ubuntu" do
+    let(:chef_run) do
+      runner = ChefSpec::ChefRunner.new(platform:'ubuntu', version:'12.04')
+      runner.converge('recipe[ntp::disable]')
+    end
+
+    it "Disables the /etc/default/ntpdate file" do
       file = chef_run.template('/etc/default/ntpdate')
       expect(file).to be_owned_by('root', 'root')
+      expect(chef_run).to create_file_with_content '/etc/default/ntpdate', "exit 0"
     end
   end
 
