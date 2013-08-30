e18af301be1ef24c75fea1c34caaeab1e9079496
Charles Johnson <charles@opscode.com>
2013-08-29 17:13:47 -0700

    Updated ntpdate_spec.rb
    Also minor refactor to ntpdate.rb recipe, making it testable.

--- spec/unit/recipes/ntpdate_spec.rb
+++ spec/unit/recipes/ntpdate_spec.rb
@@ -2,10 +2,7 @@ require 'spec_helper'
 
 describe "ntp::ntpdate" do
   let(:chef_run) do
-    runner = ChefSpec::ChefRunner.new(
-      log_level: :error,
-    )
-    Chef::Config.force_logger true
+    runner = ChefSpec::ChefRunner.new()
     runner.converge('recipe[ntp::ntpdate]')
   end
 
@@ -15,14 +12,29 @@ describe "ntp::ntpdate" do
 # only_if.
 
 # Standard test set
-  it "installs ntpdate" do
-    expect(chef_run).to install_package "ntpdate"
+  it "Does not install the ntpdate package" do
+    expect(chef_run).not_to install_package "ntpdate"
   end
 
-  it "Creates the /etc/default/ntpdate file" do
-    expect(chef_run).to create_file '/etc/default/ntpdate'
-    file = chef_run.template('/etc/default/ntpdate')
-    expect(file).to be_owned_by('root', 'root')
+  it "Does not create the /etc/default/ntpdate file" do
+    expect(chef_run).not_to create_file '/etc/default/ntpdate'
+  end
+
+  context "Ubuntu" do
+    let(:chef_run) do
+      runner = ChefSpec::ChefRunner.new(platform:'ubuntu', version:'12.04')
+      runner.converge('recipe[ntp::disable]')
+    end
+
+    it "Installs the ntpdate package" do
+      expect(chef_run).to install_package "ntpdate"
+    end
+
+    it "Creates the /etc/default/ntpdate file" do
+      file = chef_run.template('/etc/default/ntpdate')
+      expect(file).to be_owned_by('root', 'root')
+      expect(chef_run).to create_file_with_content '/etc/default/ntpdate', "0.pool.ntp.org"
+    end
   end
 
 end
