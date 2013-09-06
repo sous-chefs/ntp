40574550f185b105554aadafb4e563fe1ab662d8
Eric G. Wolfe <wolfe21@marshall.edu>
2013-09-05 21:52:27 -0400

    [COOK-3410] (rebase)
    Fixed two foodcritic warnings for the TravisCI green light, post-rebase
    
    * FC019: Access node attributes in a consistent manner: ./recipes/windows_client.rb:44
    * FC023: Prefer conditional attributes: ./recipes/windows_client.rb:33

--- recipes/windows_client.rb
+++ recipes/windows_client.rb
@@ -30,18 +30,17 @@ cookbook_file "C:/NTP/ntp.ini" do
   action :create
 end
 
-if node['kernel']['release'].to_f < 6
-  windows_package node['ntp']['vs_runtime_productname'] do
+windows_package node['ntp']['vs_runtime_productname'] do
   source node['ntp']['vs_runtime_url']
   options "/q"
   installer_type :custom
   action :install
-  end
+  only_if { node['kernel']['release'].to_f < 6 }
 end
 
 if !File.exists?("C:/NTP/bin/ntpd.exe")
   remote_file "#{Chef::Config[:file_cache_path]}/ntpd.exe" do
-    source node[:ntp][:package_url]
+    source node['ntp']['package_url']
   end
 
   execute "ntpd_install" do
