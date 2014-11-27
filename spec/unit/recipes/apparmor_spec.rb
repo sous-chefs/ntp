855eacc6d1a968d3a1b25fc0996af20d1d923c8c
Grant Ridder <grant@pagerduty.com>
2014-11-26 21:27:34 -0800

    Fix testing suite

--- spec/unit/recipes/apparmor_spec.rb
+++ spec/unit/recipes/apparmor_spec.rb
@@ -1,16 +1,17 @@
 require 'spec_helper'
 
 describe 'ntp::apparmor' do
-  let(:chef_run) { ChefSpec::ChefRunner.new.converge('recipe[ntp::apparmor]') }
+  let(:chef_run) { ChefSpec::SoloRunner.new.converge('recipe[ntp::apparmor]') }
 
   it 'creates the apparmor file' do
     expect(chef_run).to create_cookbook_file '/etc/apparmor.d/usr.sbin.ntpd'
     file = chef_run.cookbook_file('/etc/apparmor.d/usr.sbin.ntpd')
-    expect(file).to be_owned_by('root', 'root')
+    expect(file.owner).to eq('root')
+    expect(file.group).to eq('root')
   end
 
   it 'restarts the apparmor service' do
-    chef_run.cookbook_file('/etc/apparmor.d/usr.sbin.ntpd').should notify('service[apparmor]', :restart)
+    expect(chef_run.cookbook_file('/etc/apparmor.d/usr.sbin.ntpd')).to notify('service[apparmor]').to(:restart)
   end
 
 end
