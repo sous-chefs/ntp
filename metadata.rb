46fdc6c64f4faa757d1f95fced90a151b58adbd7
Tim Smith <tsmith84@gmail.com>
2015-11-17 10:43:25 -0800

    Remove attributes from the metadata
    Having these here is pretty useless

--- metadata.rb
+++ metadata.rb
@@ -14,28 +14,3 @@ end
 
 suggests 'windows'
 
-attribute 'ntp',
-          :display_name => 'NTP',
-          :description  => 'Hash of NTP attributes',
-          :type         => 'hash'
-
-attribute 'ntp/servers',
-          :display_name => 'NTP Servers',
-          :description  => 'Array of servers we should talk to',
-          :type         => 'array',
-          :default      => ['0.pool.ntp.org', '1.pool.ntp.org', '2.pool.ntp.org', '3.pool.ntp.org'],
-          :required     => 'recommended'
-
-attribute 'ntp/peers',
-          :display_name => 'NTP Peers',
-          :description  => 'Array of local NTP servers, we should peer with',
-          :type         => 'array',
-          :default      => [],
-          :required     => 'recommended'
-
-attribute 'ntp/restrictions',
-          :display_name => 'Restriction lines',
-          :description  => 'Array of restriction lines to apply to NTP servers',
-          :type         => 'array',
-          :default      => [],
-          :required     => 'recommended'
