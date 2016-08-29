3930612051f96117a798a74981483b8081dd9ca7
Tim Smith <tsmith@chef.io>
2016-08-29 12:51:48 -0700

    Testing updates
    Use cookstyle
    Remove chefdk included gems from the Gemfile
    Add chef_version metadata
    Fix failed spec
    Avoid deprecation notices in the spec
    Update platforms we test on
    Use inspecs new formatter
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -16,3 +16,5 @@ depends 'windows', '>= 1.38.0'
 
 source_url 'https://github.com/chef-cookbooks/ntp' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-cookbooks/ntp/issues' if respond_to?(:issues_url)
+
+chef_version '>= 11' if respond_to?(:chef_version)
