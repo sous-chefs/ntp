58c597cc39299b3d44784699eb853464d430a30c
Tim Smith <tsmith@chef.io>
2016-12-16 11:46:52 -0800

    Add stubs to prevent chefspec failures
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/unit/recipes/mac_os_x_client_spec.rb
+++ spec/unit/recipes/mac_os_x_client_spec.rb
@@ -3,6 +3,11 @@ require 'spec_helper'
 describe 'ntp::mac_os_x_client' do
   let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'mac_os_x', version: '10.11.1').converge('ntp::mac_os_x_client') }
 
+  before do
+    stub_command('systemsetup -getnetworktimeserver | grep -F ').and_return(false)
+    stub_command('systemsetup -getusingnetworktime | grep On').and_return(false)
+  end
+
   it 'executes systemsetup -setnetworktimeserver' do
     expect(chef_run).to run_execute('systemsetup -setnetworktimeserver')
   end
