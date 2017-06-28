36a58401da82eb2a46c77f1d795aa9c82074e04b
Tim Smith <tsmith@chef.io>
2017-06-28 10:53:50 -0700

    Update inspec tests to pass on Windows
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/integration/default/default_spec.rb
+++ test/integration/default/default_spec.rb
@@ -1,11 +1,23 @@
-describe file('/etc/ntp.conf') do
+config = if os.windows?
+           'C:\NTP\etc\ntp.conf'
+         else
+           '/etc/ntp.conf'
+         end
+
+describe file(config) do
   it { should be_file }
 end
 
-describe file('/etc/ntp.leapseconds') do
-  it { should be_file }
-end
+if os.windows?
+  describe service('NTP') do
+    it { should be_running }
+  end
+else
+  describe file('/etc/ntp.leapseconds') do
+    it { should be_file }
+  end
 
-describe command('pgrep ntp') do
-  its('exit_status') { should eq 0 }
+  describe command('pgrep ntp') do
+    its('exit_status') { should eq 0 }
+  end
 end
