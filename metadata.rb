6e6fb38cec7d7cef736791433ec636f17bbbd773
Tim Smith <tsmith84@gmail.com>
2019-10-01 10:06:54 -0700

    Resolve some cookstyle warnings
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,10 +3,7 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Installs and configures ntp as a client or server'
-
 version '3.6.2'
-
-recipe 'ntp', 'Installs and configures ntp either as a server or client'
 
 %w( amazon centos debian fedora freebsd gentoo redhat scientific solaris2 oracle ubuntu windows mac_os_x ).each do |os|
   supports os
